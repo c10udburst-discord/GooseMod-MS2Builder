518f4a74bafebc159ec4644b69260f94eba7b066
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-10-28 21:25:24 +0200

    their repo is dead so...

--- src/modules/index.js
+++ src/modules/index.js
@@ -7,7 +7,8 @@ export default [
 
     filename: 'main',
     modules: [
-      ['c10udburst-discord/GooseMod-Plugins', '1e2d15b0294bcd92bb4427eb1b25f384cc5b2ebe', '/SendEmbeds']
+      ['c10udburst-discord/GooseMod-Plugins', '1e2d15b0294bcd92bb4427eb1b25f384cc5b2ebe', '/SendEmbeds'],
+      ['xirreal/GM-Hidden-Channels', '61a36f28a869690d2f1ed028f4c93201fd6b954c'] // TODO: Remove when the repo is back online 
     ]
   },
 
