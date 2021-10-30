f1d7a4e307e965b43162e6bc618d9e00149afe62
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-10-30 19:58:19 +0200

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -9,6 +9,7 @@ export default [
     modules: [
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/SendEmbeds', undefined, { notice: 2 }],
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Token'],
+      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Sessions'],
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/EmbedMessageLinks']
     ]
   },
