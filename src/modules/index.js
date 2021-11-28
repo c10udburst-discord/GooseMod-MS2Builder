4e9856089d1d1d72799c5ebdd737c17d3295ea85
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-11-28 15:22:13 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -12,7 +12,7 @@ export default [
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Sessions'],
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/EmbedMessageLinks'],
       ['luimu64/nitro-spoof', '5a12ccd4a5f74d66aa5a79d93af599a060354d48'],
-      ['smartfrigde/GM-friend-count', '61e0bd8ada694071a59b945251eab3d8ca244974'],
+      //['smartfrigde/GM-friend-count', '61e0bd8ada694071a59b945251eab3d8ca244974'],
       ['xirreal/GM-Hidden-Channels', '728cc887158ce7a95b8425eba7d17e8f0d9c9ea6']
     ]
   },
