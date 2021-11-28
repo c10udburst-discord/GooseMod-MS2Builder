6fe917b94e8126d5261298a1d495748d9784bc15
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-11-28 16:03:17 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -14,47 +14,48 @@ export default [
       ['c10udburst-discord/GooseMod-Plugins', '', '/EmbedMessageLinks'],
       ['luimu64/nitro-spoof', ''],
       //['smartfrigde/GM-friend-count', ''],
-      ['xirreal/GM-Hidden-Channels', '', undefined, undefined, {
-		notice: 2,
-        images: [
-          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/0.png',
-          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/1.png',
-          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/2.png'
-        ]
-      }],
-      
+      ['xirreal/GM-Hidden-Channels', ''],
+
       // BdThemes
       ['discord-addons/discord-addons', '', '/download-themes-here/GreenSpotifyControls/GreenSpotifyControls.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/GreenSpotifyControls.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/GreenSpotifyControls.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/ClassicSending/ClassicSending.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/ClassicSending.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/ClassicSending.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/ServerlistAtBottom/ServerlistAtBottom.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/ServerlistAtBottom.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/ServerlistAtBottom.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/ShowFullOriginalMessageInReplies/ShowFullOriginalMessageInReplies.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/ShowFullOriginalMessageInReplies.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/ShowFullOriginalMessageInReplies.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/BlurAccountInfos/BlurAccountInfos.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/BlurAccountInfos.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/BlurAccountInfos.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/TextareaMaxHeightModifier/TextareaMaxHeightModifier.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/TextareaMaxHeightModifier.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/TextareaMaxHeightModifier.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/CompactChannels/CompactChannels.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/CompactChannels.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/CompactChannels.png']
       }],
       ['discord-addons/discord-addons', '', '/download-themes-here/TypingAtTopOfChatBox/TypingAtTopOfChatBox.theme.css', 'bdTheme', {
-        authors: ["875809253875671090"],
-        images: ['https://discord-addons.github.io/screenshots/TypingAtTopOfChatBox.png']
+	authors: ["875809253875671090"],
+	images: ['https://discord-addons.github.io/screenshots/TypingAtTopOfChatBox.png']
+      }],
+      ['Kaiidoo/Newcord', '', '/src/newcord.theme.css', 'bdTheme', {
+	authors: ["789168545488699403"],
+	images: ['https://imgur.com/oV98fvb.png', 'https://imgur.com/7VXDMbQ.png', 'https://imgur.com/MZD1Sj5.png']
+      }],
+      ['mwittrien/BetterDiscordAddons', '', '/Themes/ServerColumns/ServerColumns.theme.css', 'bdTheme', {
+	authors: ["789168545488699403"],
+	images: ['https://mwittrien.github.io/BetterDiscordAddons/Themes/ServerColumns/_res/cover.png']
       }]
     ]
   },
