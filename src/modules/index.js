ce704aa7dc338b1a2c7eb81a849fcf17c3916105
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-11-28 15:40:38 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -7,13 +7,46 @@ export default [
 
     filename: 'main',
     modules: [
-      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/SendEmbeds', undefined, { notice: 2 }],
-      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Token'],
-      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Sessions'],
-      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/EmbedMessageLinks'],
-      ['luimu64/nitro-spoof', '5a12ccd4a5f74d66aa5a79d93af599a060354d48'],
-      //['smartfrigde/GM-friend-count', '61e0bd8ada694071a59b945251eab3d8ca244974'],
-      ['xirreal/GM-Hidden-Channels', '728cc887158ce7a95b8425eba7d17e8f0d9c9ea6']
+      // GooseMod
+      ['c10udburst-discord/GooseMod-Plugins', '', '/SendEmbeds', undefined, { notice: 2 }],
+      ['c10udburst-discord/GooseMod-Plugins', '', '/Token'],
+      ['c10udburst-discord/GooseMod-Plugins', '', '/Sessions'],
+      ['c10udburst-discord/GooseMod-Plugins', '', '/EmbedMessageLinks'],
+      ['luimu64/nitro-spoof', ''],
+      //['smartfrigde/GM-friend-count', ''],
+      ['xirreal/GM-Hidden-Channels', '', undefined, undefined, {
+        images: [
+          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/0.png',
+          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/1.png',
+          'https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/2.png'
+        ]
+      }],
+      
+      // BdThemes
+      ['discord-addons/discord-addons', '', '/download-themes-here/GreenSpotifyControls/GreenSpotifyControls.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/GreenSpotifyControls.png']
+      }],
+      ['discord-addons/discord-addons', '', '/download-themes-here/ClassicSending/ClassicSending.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/ClassicSending.png']
+      }],
+      ['discord-addons/discord-addons', '', '/download-themes-here/ServerlistAtBottom/ServerlistAtBottom.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/ServerlistAtBottom.png']
+      }],
+      ['discord-addons/discord-addons', '', '/download-themes-here/ShowFullOriginalMessageInReplies/ShowFullOriginalMessageInReplies.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/ShowFullOriginalMessageInReplies.png']
+      }],
+      ['discord-addons/discord-addons', '', '/download-themes-here/BlurAccountInfos/BlurAccountInfos.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/BlurAccountInfos.png']
+      }],
+      ['discord-addons/discord-addons', '', '/download-themes-here/TextareaMaxHeightModifier/TextareaMaxHeightModifier.theme.css', 'bdTheme', {
+        authors: ["875809253875671090"],
+        images: ['https://discord-addons.github.io/screenshots/TextareaMaxHeightModifier.png']
+      }]
     ]
   },
 
