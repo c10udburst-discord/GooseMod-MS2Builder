6cc13e000488556c57d810c525f4a494c32f45e5
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2021-11-28 15:09:58 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -10,7 +10,10 @@ export default [
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/SendEmbeds', undefined, { notice: 2 }],
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Token'],
       ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/Sessions'],
-      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/EmbedMessageLinks']
+      ['c10udburst-discord/GooseMod-Plugins', '7bef277c912285ff820da8e4d7b1278ff5b81ec7', '/EmbedMessageLinks'],
+      ['luimu64/nitro-spoof', '5a12ccd4a5f74d66aa5a79d93af599a060354d48'],
+      ['smartfrigde/GM-friend-count', '61e0bd8ada694071a59b945251eab3d8ca244974'],
+      ['xirreal/GM-Hidden-Channels', '728cc887158ce7a95b8425eba7d17e8f0d9c9ea6', undefined, undefined, {"images": ["https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/0.png", "https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/1.png", "https://raw.githubusercontent.com/xirreal/GM-Hidden-Channels/main/images/2.png"] }]
     ]
   },
 
