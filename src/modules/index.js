adb887282039235ca3e8a062b099c04373a53026
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-02-25 22:47:58 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -11,14 +11,19 @@ export default [
       ['c10udburst-discord/GooseMod-Plugins', '', '/SendEmbeds', undefined, { notice: 2 }],
       ['c10udburst-discord/GooseMod-Plugins', '', '/Token'],
       ['c10udburst-discord/GooseMod-Plugins', '', '/Sessions'],
-      ['luimu64/nitro-spoof', ''],
+      //['luimu64/nitro-spoof', ''],
       //['smartfrigde/GM-friend-count', ''],
-      ['xirreal/GM-Hidden-Channels', ''],
+      //['xirreal/GM-Hidden-Channels', ''],
+
+      // PCThemes
+      ['leeprky/MaterialYouTheme', '', '/powercord_manifest.json', 'pcTheme'],
 	    
       // PCPlugins
       ['CaptainBoggle/Streamiun', '', '/manifest.json', 'pcPlugin', {
       	authors: ["306249172032552980"]
       }],
+      ['notsapinho/powercord-together', '', '/manifest.json', 'pcPlugin'],
+      ['powercord-community/multitask', '', '/manifest.json', 'pcPlugin'],
 
       // BdThemes
       ['discord-addons/discord-addons', '', '/download-themes-here/ClassicSending/ClassicSending.theme.css', 'bdTheme', {
@@ -45,9 +50,9 @@ export default [
 	authors: ["875809253875671090"],
 	images: ['https://discord-addons.github.io/screenshots/CompactChannels.png']
       }],
-      ['mwittrien/BetterDiscordAddons', '', '/Themes/ServerColumns/ServerColumns.theme.css', 'bdTheme', {
-	images: ['https://mwittrien.github.io/BetterDiscordAddons/Themes/ServerColumns/_res/cover.png']
-      }]
+      //['mwittrien/BetterDiscordAddons', '', '/Themes/ServerColumns/ServerColumns.theme.css', 'bdTheme', {
+      // images: ['https://mwittrien.github.io/BetterDiscordAddons/Themes/ServerColumns/_res/cover.png']
+      //}]
     ]
   },
 
