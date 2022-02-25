c1261f4a8d9a22ff983a8ca45dbce02b037f6d83
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-02-25 23:08:15 +0100

    asdf

--- src/modules/index.js
+++ src/modules/index.js
@@ -10,45 +10,13 @@ export default [
       // GooseMod
       ['c10udburst-discord/GooseMod-Plugins', '', '/Token'],
       ['c10udburst-discord/GooseMod-Plugins', '', '/Sessions'],
-      //['luimu64/nitro-spoof', ''],
-      //['smartfrigde/GM-friend-count', ''],
-      //['xirreal/GM-Hidden-Channels', ''],
-	    
+
       // PCPlugins
       ['CaptainBoggle/Streamiun', '', '/manifest.json', 'pcPlugin', {
-      	authors: ["306249172032552980"]
+        authors: ["306249172032552980"]
       }],
       ['notsapinho/powercord-together', '', '/manifest.json', 'pcPlugin'],
 
-      // BdThemes
-	['leeprky/MaterialYouTheme', '', '/betterdiscord/MaterialYouTheme.theme.css', 'bdTheme']
- //     ['discord-addons/discord-addons', '', '/download-themes-here/ClassicSending/ClassicSending.theme.css', 'bdTheme', {
-//  authors: ["875809253875671090"],
-//	images: ['https://discord-addons.github.io/screenshots/ClassicSending.png']
- //     }],
-  //    ['discord-addons/discord-addons', '', '/download-themes-here/ServerlistAtBottom/ServerlistAtBottom.theme.css', 'bdTheme', {
-//authors: ["875809253875671090"],
-//	images: ['https://discord-addons.github.io/screenshots/ServerlistAtBottom.png']
-   //   }],
-    //  ['discord-addons/discord-addons', '', '/download-themes-here/ShowFullOriginalMessageInReplies/ShowFullOriginalMessageInReplies.theme.css', 'bdTheme', {
-	//authors: ["875809253875671090"],
-//	images: ['https://discord-addons.github.io/screenshots/ShowFullOriginalMessageInReplies.png']
-   //   }],
-      //['discord-addons/discord-addons', '', '/download-themes-here/BlurAccountInfos/BlurAccountInfos.theme.css', 'bdTheme', {
-	//authors: ["875809253875671090"],
-	//images: ['https://discord-addons.github.io/screenshots/BlurAccountInfos.png']
-      //}],
-      //['discord-addons/discord-addons', '', '/download-themes-here/TextareaMaxHeightModifier/TextareaMaxHeightModifier.theme.css', 'bdTheme', {
-	//authors: ["875809253875671090"],
-	//images: ['https://discord-addons.github.io/screenshots/TextareaMaxHeightModifier.png']
-     // }],
-     // ['discord-addons/discord-addons', '', '/download-themes-here/CompactChannels/CompactChannels.theme.css', 'bdTheme', {
-	//authors: ["875809253875671090"],
-	//images: ['https://discord-addons.github.io/screenshots/CompactChannels.png']
-     // }],
-      //['mwittrien/BetterDiscordAddons', '', '/Themes/ServerColumns/ServerColumns.theme.css', 'bdTheme', {
-      // images: ['https://mwittrien.github.io/BetterDiscordAddons/Themes/ServerColumns/_res/cover.png']
-      //}]
     ]
   },
 
